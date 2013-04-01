884d7d24fecaf5680cdd208f93f5e4b817c03b51
Michael Shick <michael.shick@gmail.com>
2013-04-01 18:56:26 -0400

    many enhancements thanks to http://justinlilly.com/dotfiles/zsh.html

---
+++ osx/aliases.zsh
@@ -0,0 +1,5 @@
+# Thanks to: http://justinlilly.com/dotfiles/zsh.html
+
+# Mac Helpers
+alias show_hidden="defaults write com.apple.Finder AppleShowAllFiles YES && killall Finder"
+alias hide_hidden="defaults write com.apple.Finder AppleShowAllFiles NO && killall Finder"
