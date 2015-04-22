9cdad1ce49a3f2a43b921b62608da91e38558cee
Michael Shick <michael.shick@gmail.com>
2015-04-21 22:55:47 -0400

    don't bother installing nvm. pita.

--- node/install.sh
+++ node/install.sh
@@ -7,16 +7,8 @@ fi
 
 if test ! $(which node)
 then
-    echo "  Installing io.js & node using nvm for you. [io.js is default]"
-    brew install nvm > /tmp/nvm-install.log
-
-    export NVM_DIR=$nvm_dir
-    source $(brew --prefix nvm)/nvm.sh
-
-    nvm install node > /tmp/node-install.log
-    nvm install iojs > /tmp/iojs-install.log
-    nvm use iojs
-    nvm alias default iojs
+    echo "  Installing node.js for you."
+    brew install node > /tmp/node-install.log
 fi
 
 npm="$(brew --prefix)/lib/node_modules/npm"
@@ -31,12 +23,6 @@ if test ! $(which nodemon)
 then
     echo "  Installing nodemon for you."
     $npm install nodemon > /tmp/nodemon-install.log
-fi
-
-if test ! $(which gulp)
-then
-    echo "  Installing gulp for you."
-    $npm install gulp > /tmp/gulp-install.log
 fi
 
 exit 0
