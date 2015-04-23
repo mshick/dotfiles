23bcf20da71f4598b0d38861ed9ea1a1df0e4ada
Michael Shick <michael.shick@gmail.com>
2015-04-23 15:13:34 -0400

    attempting to clean up sublime install

--- sublime-text-3/install.sh
+++ sublime-text-3/install.sh
@@ -8,46 +8,64 @@ then
     exit;
 fi
 
-install_sublime () {
+source "${ZSH}/util/interactive"
+
+ST3=~/Library/'Application Support'/'Sublime Text 3'
+
+# install_sublime () {
     # Run this script with:
     # $ curl -L git.io/sublimetext | sh
-    curl -o ~/st3.dmg $URL
-    mountpoint="/Volumes/Sublime Text"
-    IFS="
-    "
-    hdiutil attach -mountpoint $mountpoint ~/st3.dmg
+#     curl -o ~/st3.dmg $URL
+#     mountpoint="/Volumes/Sublime Text"
+#     IFS="
+#     "
+#     hdiutil attach -mountpoint $mountpoint ~/st3.dmg
 
-    for app in `find $mountpoint -type d -maxdepth 2 -name \*.app `; do
-      cp -a "$app" /Applications/
-    done
+#     for app in `find $mountpoint -type d -maxdepth 2 -name \*.app `; do
+#       cp -a "$app" /Applications/
+#     done
 
-    hdiutil detach $mountpoint
-    rm ~/st3.dmg
+#     hdiutil detach $mountpoint
+#     rm ~/st3.dmg
+# }
+
+link_config() {
+    if [[ -d "${ST3}/Installed Packages" ]]
+    then
+       mv "${ST3}/Installed Packages" "${ST3}/Installed Packages.bak"
+    fi
+
+    if [[ -d "${ST3}/Packages" ]]
+    then
+        mv "${ST3}/Packages" "${ST3}/Packages.bak"
+    fi
+
+    if [[ -e "${ST3}/Installed Packages" ]]
+    then
+        ln -s "${ZSH}/sublime-text-3/Installed Packages" "${ST3}/Installed Packages"
+    fi
+
+    if [[ -e "${ST3}/Installed Packages" ]]
+    then
+        ln -s "${ZSH}/sublime-text-3/Packages" "${ST3}/Packages"
+    fi
 }
 
 if ! [[ -e "~/Applications/Sublime Text.app" ]]
 then
+    info "Installing Sublime Text"
+
     # install_sublime
     brew cask install sublime-text3 --force
 
     # Symlink config
-    st3=~/Library/'Application Support'/'Sublime Text 3'
-    if [[ ! -d $st3 ]]; then
-        mkdir $st3
+    if [[ ! -d $ST3 ]]; then
+        mkdir $ST3
     fi
 
-    if [[ -d "${st3}/Installed Packages" ]]
-    then
-       mv "${st3}/Installed Packages" "${st3}/Installed Packages.bak"
-    fi
+    info "Linking Sublime configuration files"
 
-    if [[ -d "${st3}/Packages" ]]
-    then
-        mv "${st3}/Packages" "${st3}/Packages.bak"
-    fi
-
-    ln -s "${ZSH}/sublime-text-3/Installed Packages" "${st3}/Installed Packages"
-    ln -s "${ZSH}/sublime-text-3/Packages" "${st3}/Packages"
+    link_config()
 
     echo ""
     echo "Sublime Text 3 installed successfully!"
