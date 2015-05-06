b27f1bf16496de008ccb60b223d697f3d99b58dc
Michael Shick <michael.shick@gmail.com>
2015-05-06 11:47:37 -0400

    adding docker install

---
+++ docker/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ ! "$(uname -s)" == "Darwin" ]
+then
+    exit;
+fi
+
+if test ! $(which docker) && ! [[ $(brew ls --versions docker) ]]
+then
+  echo "  Installing docker for you."
+  brew install docker > /tmp/docker-install.log
+fi
+
+if test ! $(which boot2docker) && ! [[ $(brew ls --versions boot2docker) ]]
+then
+  echo "  Installing boot2docker for you."
+  brew install boot2docker > /tmp/boot2docker-install.log
+fi
+
+exit 0
