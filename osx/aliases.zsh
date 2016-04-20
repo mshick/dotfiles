422c3e25a21a32b97010f419a34016c9f47eb958
Michael Shick <m@shick.us>
2016-04-19 21:05:50 -0700

    fixscreen alias

---
+++ osx/aliases.zsh
@@ -0,0 +1,1 @@
+alias fixscreen='sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.screensharing.plist &&  sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist'
