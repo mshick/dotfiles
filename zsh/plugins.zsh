9696982808359798a5df4298f0a0098c91e56132
mshick <m@shick.us>
2019-02-27 16:38:32 -0500

    fix zsh history

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -8,6 +8,8 @@ syntax_path="$(brew --prefix zsh-syntax-highlighting)/share/zsh-syntax-highlight
 if [[ -d $syntax_path ]]; then
     source "${syntax_path}/zsh-syntax-highlighting.zsh"
     export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR="${syntax_path}/highlighters"
+else
+    echo "zsh-syntax-highlighting not installed"
 fi
 
 
@@ -16,7 +18,7 @@ fi
 # Sourced from brew installed code
 # ----------------------------------------------------------------------
 
-history_path="$(brew --prefix zsh-history-substring-search)/share/zsh-history-substring-search"
+history_path="$(brew --prefix zsh-history-substring-search)"
 
 if [[ -d $history_path ]]; then
     source "${history_path}/zsh-history-substring-search.zsh"
@@ -24,4 +26,6 @@ if [[ -d $history_path ]]; then
     # bind UP and DOWN arrow keys
     bindkey '^[[A' history-substring-search-up
     bindkey '^[[B' history-substring-search-down
+else
+    echo "zsh-history-substring-search not installed"
 fi
