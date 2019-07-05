c57ffe2b0a7f213bc8923fdd6e763533bf9ed6c3
Michael Shick <m@shick.us>
2019-07-05 15:10:47 -0400

    add watchman based copy movies

---
+++ server-scripts/copy-movies.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source_folder=/Users/mshick/test
+destination_folder=/Users/mshick/dest
+
+files=( $(/usr/local/bin/watchman since "${source_folder}" n:movies -- -I "**/*.mkv" | /usr/local/bin/jq -r '.files | map(select (.new == true)) | map(.name) | join(" ")') )
+
+echo  "[$(date)] Checked for new movies in ${source_folder}"
+
+for file in "${files[@]}"; do
+    src="${source_folder}/${file}"
+    dst="${destination_folder}/${file}"
+
+    echo "[$(date)] Copying file ${src} to ${dst}"
+
+    cp "${source_folder}/${file}" "${destination_folder}/${file}"
+done
