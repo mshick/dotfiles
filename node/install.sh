787fa8bc2c19c2be4630bcb9ad00e5acbbf27ded
Michael Shick <m@shick.us>
2020-02-26 12:19:42 -0500

    feat: add yarn install for node

--- node/install.sh
+++ node/install.sh
@@ -5,3 +5,9 @@ then
     echo "  Installing name-that-color for you."
     npm install name-that-color --global > /tmp/name-that-color-install.log
 fi
+
+if test ! $(which yarn)
+then
+    echo "  Installing yarn for you."
+    npm install yarn --global > /tmp/yarn-install.log
+fi
