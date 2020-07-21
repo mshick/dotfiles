841596257baf39289975265d12168a3e160737ab
Michael Shick <m@shick.us>
2020-07-21 17:06:52 -0400

    remove yarn usage

--- node/install.sh
+++ node/install.sh
@@ -7,25 +7,25 @@ info "running node/install"
 if test ! $(which name-that-color)
 then
     detail "name-that-color not found, installing"
-    yarn global add name-that-color > /tmp/name-that-color-install.log
+    npm install --global name-that-color > /tmp/name-that-color-install.log
 fi
 
 if test ! $(which npm-check-updates)
 then
     detail "npm-check-updates not found, installing"
-    yarn global add npm-check-updates > /tmp/npm-check-updates-install.log
+    npm install --global npm-check-updates > /tmp/npm-check-updates-install.log
 fi
 
 if test ! $(which tsc)
 then
     detail "typescript not found, installing"
-    yarn global add typescript > /tmp/typescript-install.log
+    npm install --global typescript > /tmp/typescript-install.log
 fi
 
 if test ! $(which react-devtools)
 then
     detail "react-devtools not found, installing"
-    yarn global add react-devtools > /tmp/react-devtools-install.log
+    npm install --global react-devtools > /tmp/react-devtools-install.log
 fi
 
 success "node/install"
